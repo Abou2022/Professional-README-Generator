af24de459eab510bab141c30da6541070e50f573
Bakary Sylla <syllabakary2002@gmail.com>
2022-04-21 23:45:47 -0700

    Final

--- Readme/readme.md
+++ Readme/readme.md
@@ -1,13 +1,10 @@
 
-  # https://github.com/Abou2022 /undefined / readme prokjec 
-  * help to generate easy readme section
-  * insat node 
-  * terminal
-  * n/a
-  * node inquire
-
-  * Thank you
-  * syllabakary@gmail.com
-  *
-
+  # https://github.com/Abou2022 /undefined / professional README generator 
+  * Description: You can quickly and easily create a README file by using a command-line application to generate one. 
+  * Installation: node.js inquirer fs
+  * Usage : open the terminal to run the programme
+  * Contibution : N/A
+  * Test:node.js fs inquirer
+  * Question 
+  * For any question, you can eamil at :syllabakary2002@gmail.com
   
--- index.js
+++ index.js
@@ -1,88 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-// inquirer
-//   .prompt([
-//     {
-//       type: "input",
-//       name: "title",
-//       message: "What is your project title?",
-//     },
-//     {
-//       type: "input",
-//       name: "description",
-//       message: "describe your project?",
-//     },
-//     {
-//       type: "input",
-//       name: "installation",
-//       message: "Installation Instruction? ",
-//     },
-//     {
-//       type: "input",
-//       name: "usage",
-//       message: "How to use the application?",
-//     },
-//     {
-//       type: "input",
-//       name: "contribution",
-//       message: "What is the contribution guidelines?",
-//     },
-//     {
-//       type: "input",
-//       name: "test",
-//       message: "What is the instruction for the test?",
-//     },
-//   ])
-//   .then(function ({
-//     title,
-//     description,
-//     installation,
-//     usage,
-//     contribution,
-//     test,
-//   }) {
-//     // console.log(title, description, installation, usage, contribution, test);
-
-//     let generateREADME = `
-//     <!DOCTYPE html>
-// <html lang="en">
-//   <head>
-//     <meta charset="UTF-8" />
-//     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//     <title>Readme Generator</title>
-//     <link rel="stylesheet" href="./style.css" />
-//   </head>
-//   <body>
-//     <h1>Project : ${title}</h1>
-//     <section>
-//       <div>
-//         <h3>## Project description</h3>
-//         <p>${description}</p>
-
-//         <h3>## Installation instructions</h3>
-//         <p>${installation}</p>
-
-//         <h3>## Usage information</h3>
-//         <p>${usage}</p>
-
-//         <h3>## Contribution guidelines</h3>
-//         <p>${contribution}</p>
-
-//         <h3>## Test instructions</h3>
-//         <p>${test}</p>
-//       </div>
-//     </section>
-//   </body>
-// </html>
-
-//     `;
-//     console.log(generateREADME);
-
-//     fs.writeFileSync(`./Readme/${title}.html`, generateREADME);
-//   });
-
 function prompt() {
   return inquirer.prompt([
     {
@@ -151,12 +69,12 @@ function generateREADME({
 }) {
   let ReadMe = `
   # https://github.com/${github} /${license} / ${title} 
-  * ${description}
-  * ${installation}
-  * ${usage}
-  * ${contribution}
-  * ${test}
-  * ${question}
+  * Description: ${description}
+  * Installation: ${installation}
+  * Usage : ${usage}
+  * Contibution : ${contribution}
+  * Test:${test}
+  * Question ${question}
   * For any question, you can eamil at :${email}
   `;
 
