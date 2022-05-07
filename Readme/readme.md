285526b05c89017ec72c6770f010e2dcf4be9339
Bakary Sylla <syllabakary2002@gmail.com>
2022-05-06 20:43:01 -0700

    WIP

--- Readme/readme.md
+++ Readme/readme.md
@@ -1,58 +1,50 @@
 
-  #  Professional read me [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+#  readMe ![License:](https://img.shields.io/badge/License-MIT-yellow.svg)
 
-  ## Table of Contents:
-  1. Descriptions
-  2. Installation
-  3. Usage
-  4. Contribution
-  5. Test
-  6. License
-  7. Github
-  8.Question
-  9. Eamil
 
-  ## Description 
+    ## Table of Contents:
 
-  This project will help you to build a professional read me with all composante
+    1. Descriptions
+    2. Installation
+    3. Usage
+    4. Contribution
+    5. Test
+    6. License
+    7. Github
+    8.Question
+    9. Eamil
 
-  ## Installation 
+ ## Description 
+bfebfjkbfwebijf
 
-  To run the program, use the command npm i to install all dependencies.
+## Installation 
+To run the program, use the command fbfbwekwbekbe to install all dependencies.
 
-  ## Usage 
+ ## Usage 
+ fejkbfkefbkefbkewf
 
-   this application used node.js, inquirer package and fs
+## Contibution
+ewfhhewfkhef j h wc
 
-  ## Contibution
+ ## Test
+chwbfbwh h vh w
 
-  By email, or pull request
+## License
+MIT License
+CopyrightMIT ![License:](https://img.shields.io/badge/License-MIT-yellow.svg) 
 
-  ## Test
-
-  npm test
-
-  ## License
-  MIT License
-  Copyright undefined Bakary Sylla
-
+Bakary Sylla
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    
+## Github
+For Any question or request you can to on my [Github](https://github.com/hfkbwbwfbwjkr)
 
-  
+## Question
+undefined
 
-  ## Github
-
-  For Any question or request you can to on my [Github](https://github.com/Abou2022)
-
-  ## Question
-   undefined
-
-   ## Email
-
-   For any question, you can eamil at : 
-   [email](mailto:{syllabakary@gmail.com}
-  
+## Email
+For any question, you can eamil at : 
+[email](mailto:ejfjn@bfbij.com)
+    
--- index.js
+++ index.js
@@ -1,146 +1,130 @@
 
-
-// function init(){
-
-const inquirer = require("inquirer");
-const fs = require("fs");
-
-
-function prompt() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "title",
-      message: "What is your project title?",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "What is the describtion of your project?",
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: "What command should be run to install dependencies? ",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "How to use the application?",
-    },
-    {
-      type: "input",
-      name: "contribution",
-      message: "What is the contribution guidelines?",
-    },
-    {
-      type: "input",
-      name: "test",
-      message: "What command should be used to run the test?",
-    },
-    {
-      type: "input",
-      name: " license",
-      message: "What license year?",
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "What is your gitub userName?",
-    },
-    // {
-    //   type: "input",
-    //   name: "question",
-    //   message: "For any question contact me on Github",
-    // },
-    {
-      type: "input",
-      name: "email",
-      message: "what is your email addrress if I want to conatct you",
-    },
-  ]);
-}
-function generateREADME({
-  title,
-  description,
-  installation,
-  usage,
-  contribution,
-  test,
-  license,
-  github,
-  question,
-  email,
-}) {
-  let ReadMe = `
-  #  ${title} [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-
-  ## Table of Contents:
-  1. Descriptions
-  2. Installation
-  3. Usage
-  4. Contribution
-  5. Test
-  6. License
-  7. Github
-  8.Question
-  9. Eamil
-
-  ## Description 
-
-  ${description}
-
-  ## Installation 
-
-  To run the program, use the command ${installation} to install all dependencies.
-
-  ## Usage 
-
-  ${usage}
-
-  ## Contibution
-
-  ${contribution}
-
-  ## Test
-
-  ${test}
-
-  ## License
-  MIT License
-  Copyright ${license} Bakary Sylla
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+  const inquirer = require("inquirer");
+  const fs = require("fs");
   
+  
+  function prompt() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is your project title?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "What is the describtion of your project?",
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "What command should be run to install dependencies? ",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "How to use the application?",
+      },
+      {
+        type: "input",
+        name: "contribution",
+        message: "What is the contribution guidelines?",
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "What command should be used to run the test?",
+      },
+      {
+        type: "input",
+        name: "license",
+        message: "What is the license ? (i.e ... MIT, GNU LGPLv3, Apache License 2.0)",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your gitub userName?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "This is my email addrress if you want to conatct you",
+      },
+    ]);
+  }
+  function generateREADME({
+    title,
+    description,
+    installation,
+    usage,
+    contribution,
+    test,
+    license,
+    github,
+    question,
+    email,
+  }) {
+    let ReadMe = `
+#  ${title} ![License:](https://img.shields.io/badge/License-${license}-yellow.svg)
 
-  ## Github
 
-  For Any question or request you can to on my [Github](https://github.com/${github})
+    ## Table of Contents:
 
-  ## Question
-   ${question}
+    1. Descriptions
+    2. Installation
+    3. Usage
+    4. Contribution
+    5. Test
+    6. License
+    7. Github
+    8.Question
+    9. Eamil
 
-   ## Email
+ ## Description 
+${description}
 
-   For any question, you can eamil at : 
-   [email](mailto:{${email}}
-  `;
+## Installation 
+To run the program, use the command ${installation} to install all dependencies.
 
-  return ReadMe;
-}
+ ## Usage 
+ ${usage}
 
-function WriteReadmeFile(ReadMe) {
-  fs.writeFileSync(`./Readme/readme.md`, ReadMe);
-}
-async function init() {
-  let answers = await prompt();
-  let genREADME = generateREADME(answers);
-  WriteReadmeFile(genREADME);
-}
+## Contibution
+${contribution}
 
-init();
+ ## Test
+${test}
+
+## License
+${license} License
+Copyright ${license} ![License:](https://img.shields.io/badge/License-${license}-yellow.svg) 
+
+Bakary Sylla
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    
+## Github
+For Any question or request you can to on my [Github](https://github.com/${github})
+
+## Question
+${question}
+
+## Email
+For any question, you can eamil at : 
+[email](mailto:${email})
+    `;
+  
+    return ReadMe;
+  }
+  
+  function WriteReadmeFile(ReadMe) {
+    fs.writeFileSync(`./Readme/readme.md`, ReadMe);
+  }
+  async function init() {
+    let answers = await prompt();
+    let genREADME = generateREADME(answers);
+    WriteReadmeFile(genREADME);
+  }
+  
+  init();
